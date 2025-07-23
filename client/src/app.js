0872414e8276d9495c63f29ba44f3037334cd93d
하헌찬 <gjscks0106@gmail.com>
2025-07-24 03:23:23 +0900

    refactor: practice와 client파일 형식 통합

--- client/src/app.js
+++ client/src/app.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import TrashReportBoard from './components/complain';
-// import MainPage from './pages/MainPage';  // 추후 메인화면 필요 시 사용
-// import IncentivePage from './pages/IncentivePage'; // 인센티브 페이지 예시
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import TrashReportBoard from './components/complain/complain';
+import WasteSorting from './components/trashsort_ai/trashsort_ai';
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* 기본 경로는 /complain으로 리다이렉트 */}
-        <Route path="/" element={<Navigate to="/complain" replace />} />
+        {/* "/" 경로는 쓰레기 분류 메인 페이지 */}
+        <Route path="/" element={<WasteSorting />} />
 
         {/* 민원 게시판 경로 */}
         <Route path="/complain" element={<TrashReportBoard />} />
